558c2017c4ef52baf4ccdd63d5e0ca28d0b7a3ea
Fluffy-Lion <leoncooper31@gmail.com>
2021-06-07 14:38:25 +0100

    Updates

---
+++ precache-manifest.a35bbf598f3a166a2330180e9b4138f5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecc1cf1fd1564512d54266d1debb420",
+    "url": "/card-match/index.html"
+  },
+  {
+    "revision": "db0c301ce048f3e49bd5",
+    "url": "/card-match/static/css/main.aa2dde34.chunk.css"
+  },
+  {
+    "revision": "8ad691b78bfeb8fcab77",
+    "url": "/card-match/static/js/2.a62e5c73.chunk.js"
+  },
+  {
+    "revision": "db0c301ce048f3e49bd5",
+    "url": "/card-match/static/js/main.2e6d190f.chunk.js"
+  },
+  {
+    "revision": "58c62a05d49d756f7075",
+    "url": "/card-match/static/js/runtime-main.d8eee0ac.js"
+  },
+  {
+    "revision": "457556db2f1c7716bbf7781e84462a32",
+    "url": "/card-match/static/media/Stu.457556db.jpg"
+  },
+  {
+    "revision": "761ed6d3328ed8b4738639491ed73846",
+    "url": "/card-match/static/media/andy.761ed6d3.jpg"
+  },
+  {
+    "revision": "db7d3b5e25130cf6c6e0eb0a2d64f688",
+    "url": "/card-match/static/media/ben.db7d3b5e.jpeg"
+  },
+  {
+    "revision": "b6f6971a4f980d45989a8ff46f9c78b7",
+    "url": "/card-match/static/media/dan.b6f6971a.jpg"
+  },
+  {
+    "revision": "64daaae8d2bd4a2bf998d174c42dd5ec",
+    "url": "/card-match/static/media/dan2.64daaae8.jpg"
+  },
+  {
+    "revision": "f919335f799666e9da57a066c0f8a3bd",
+    "url": "/card-match/static/media/dave.f919335f.jpg"
+  },
+  {
+    "revision": "01bee9655bf434da91a4f4f5eed32339",
+    "url": "/card-match/static/media/gilroy-bold.01bee965.otf"
+  },
+  {
+    "revision": "d90fa984926d3745c7e68bc525692c26",
+    "url": "/card-match/static/media/gilroy.d90fa984.otf"
+  },
+  {
+    "revision": "f01fe2a72ee48e9c269191684cf62f62",
+    "url": "/card-match/static/media/liam.f01fe2a7.jpg"
+  },
+  {
+    "revision": "738bce8fcc3f58c3872da20cf75b31c4",
+    "url": "/card-match/static/media/logo.738bce8f.png"
+  },
+  {
+    "revision": "f9e34f9236442a0604dc80460b5a1372",
+    "url": "/card-match/static/media/rach.f9e34f92.jpg"
+  },
+  {
+    "revision": "d537f27cb0fc5a1d3a1003352bbce947",
+    "url": "/card-match/static/media/robbie.d537f27c.jpeg"
+  }
+]);
